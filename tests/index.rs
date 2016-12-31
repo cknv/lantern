a721325a5a9735b1e86bbde2197c4ce63be198d4
Esben Sonne <esbensonne@gmail.com>
2016-12-31 10:26:29 +0100

    Updated test to use tempdir

--- tests/index.rs
+++ tests/index.rs
@@ -1,12 +1,13 @@
 extern crate lucerne;
+extern crate tempdir;
 
-use std::path::Path;
+use tempdir::TempDir;
 
 
 #[test]
 fn it_works() {
-    let base_path = Path::new("./");
-    let mut index = lucerne::Index::new(base_path);
+    let base_path = TempDir::new("index-path").unwrap();
+    let mut index = lucerne::Index::new(base_path.path());
     index.create("id_1", "this is some text");
     assert!(index.search("some").contains("id_1"));
 }
